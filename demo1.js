878ed00d7c937d7fc95af08699e1de5f47450264
sumitsharma012 <sumitsharmasam1997@gmail.com>
2016-12-30 14:11:51 +0530

    Add files via upload

---
+++ demo1.js
@@ -0,0 +1,25 @@
+var a;
+var b;
+var c;
+
+var a=5;
+console.log(a);
+
+function add() {
+  var a;
+  var b;
+  console.log(a);
+  console.log(c);
+  var  a=3;
+  var b=6;
+  console.log(b);
+  console.log(a);
+}
+var c=8;
+var d=5;
+add();
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
---
+++ demo2.js
@@ -0,0 +1,11 @@
+var a;
+function add() {
+  console.log(a);
+  var a=5;
+  console.log(b);
+  var b=2;
+  console.log(a);
+  console.log(b);
+}
+add();
+console.log(b);
